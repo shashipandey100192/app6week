a12ea8ddbc78f940c4753dfb7ec7831f0c13912d
shashi <shashiweb64@gmail.com>
2026-07-18 17:33:35 +0530

    change

--- src/modules/users/auth/Userregistor.js
+++ src/modules/users/auth/Userregistor.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { useForm } from 'react-hook-form'
+import axios from 'axios';
 function Userregistor() {
+
+ const {register,handleSubmit,formState: { errors }} = useForm();
+
+const myform = (d)=>{
+    console.log(d)
+    axios.post("http://localhost:8900/registor",d).then((r)=>{
+        console.log(r);
+        if(r.data.status===211)
+        {
+            alert(r.data.msg);
+            
+        }
+    })
+}
+
+
   return (
+    <form onSubmit={handleSubmit(myform)}>
    <div className='container'>
             <div className='row justify-content-center'>
                 <div className='col-md-8 border bg-light shadow p-4'>
@@ -15,19 +33,19 @@ function Userregistor() {
                             <div className='col-md-6'>
                                 <div class="mb-3">
                                     <label class="form-label">Email address</label>
-                                    <input type="email" class="form-control" />
+                                    <input type="email" class="form-control" {...register("email")} name='email'/>
                                 </div>
                             </div>
                             <div className='col-md-6'>
                                 <div class="mb-3">
                                     <label class="form-label">Phone</label>
-                                    <input type="text" class="form-control" />
+                                    <input type="text" class="form-control" {...register("phone")} name='phone'/>
                                 </div>
                             </div>
                             <div className='col-md-6'>
                                 <div class="mb-3">
                                     <label class="form-label">Gender</label>
-                                    <select className='form-select'>
+                                    <select className='form-select' {...register("gender")} name='gender'>
                                       <option>Gender</option>
                                       <option>male</option>
                                       <option>female</option>
@@ -37,13 +55,13 @@ function Userregistor() {
                             <div className='col-md-6'>
                                 <div class="mb-3">
                                     <label class="form-label">Password</label>
-                                    <input type="password" class="form-control" />
+                                    <input type="password" class="form-control" {...register("pass")} name='pass'/>
                                 </div>
                             </div>
                             <div className='col-12 text-center'>
                                 <div class="mb-3">
                                    
-                                    <input type="button"  value="login" class="btn btn-success" />
+                                    <input type="submit"  value="login" class="btn btn-success" />
                                     {/* <Link to='registor' className='ms-5'>New Registor </Link> */}
                                 </div>
                             </div>
@@ -54,6 +72,7 @@ function Userregistor() {
                 </div>
             </div>
         </div>
+        </form>
   )
 }
 
